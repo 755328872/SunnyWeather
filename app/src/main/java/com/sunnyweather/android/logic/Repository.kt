1994142c7e0b61ae695df9b683bafc028ed24331
755328872 <755328872@qq.com>
2021-09-01 21:04:57 +0800

    加入显示天气信息功能

--- app/src/main/java/com/sunnyweather/android/logic/Repository.kt
+++ app/src/main/java/com/sunnyweather/android/logic/Repository.kt
@@ -1,11 +1,14 @@
 package com.sunnyweather.android.logic
 
 import androidx.lifecycle.liveData
+import com.sunnyweather.android.logic.dao.PlaceDao
+//import com.google.android.gms.awareness.state.Weather
+//import com.sunnyweather.android.logic.model.Weather
 import com.sunnyweather.android.logic.model.Place
-import com.sunnyweather.android.logic.model.PlaceResponse
 import com.sunnyweather.android.logic.network.SunnyWeatherNetwork
 import kotlinx.coroutines.Dispatchers
-import okhttp3.Dispatcher
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import java.lang.Exception
 import java.lang.RuntimeException
 
@@ -25,4 +28,42 @@ object Repository {
         }
         emit(result)
     }
+
+    fun refreshWeather(lng: String, lat: String, placeName: String) = liveData(Dispatchers.IO) {
+        val result = try {
+            coroutineScope {
+                val deferredRealtime = async {
+                    SunnyWeatherNetwork.getRealtimeWeather(lng, lat)
+                }
+                val deferredDaily = async {
+                    SunnyWeatherNetwork.getDailyWeather(lng, lat)
+                }
+                val realtimeResponse = deferredRealtime.await()
+                val dailyResponse = deferredDaily.await()
+                if (realtimeResponse.status == "ok" && dailyResponse.status == "ok") {
+                    val weather = com.sunnyweather.android.logic.model.Weather(
+                        realtimeResponse.result.realtime,
+                        dailyResponse.result.daily
+                    )
+                    Result.success(weather)
+                } else {
+                    Result.failure(
+                        RuntimeException(
+                            "realtime response status is ${realtimeResponse.status}" +
+                                    "daily response status is ${dailyResponse.status}"
+                        )
+                    )
+                }
+            }
+        } catch (e: Exception) {
+            Result.failure<com.sunnyweather.android.logic.model.Weather>(e)
+        }
+        emit(result)
+    }
+
+    fun savePlace(place: Place) = PlaceDao.savePlace(place)
+
+    fun getSavedPlace() = PlaceDao.getSavedPlace()
+
+    fun isPlaceSaved() = PlaceDao.isPlaceSaved()
 }
